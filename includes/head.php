758bce154c3d61c716ad7b288824eb89c12451d1
Nic Bertino <nbertino@scu.edu>
2015-08-10 17:28:59 -0700

    Initial commit from babywhale

---
+++ includes/head.php
@@ -0,0 +1,11 @@
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap-scu/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/scu.css" rel="stylesheet">
+    <script src="js/modernizr.min.js"></script>
+    
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <link href="/css/ie.css" rel="stylesheet" type="text/css"/>
+      <script src="/js/html5shiv.min.js"></script>
+      <script src="/js/respond.min.js"></script>
+    <![endif]-->
---
+++ includes/header.php
@@ -0,0 +1,150 @@
+	<a class="sr-only sr-only-focusable" href="#content">Skip to main content</a>
+    <header class="site-header uses-core-navigation site-header-with-seal">
+      <div class="site-header-text-and-search">
+        <div class="container">
+          <div class="row">
+            <div class="col-xs-12 col-sm-9 site-header-titles">
+              <a href="index.php" class="text-hide pull-left seal">Santa Clara University</a>
+              <a href="index.php" class="wordmarks"><img src="css/assets/header-large-Santa-Clara-University.png" srcset="css/assets/header-large-Santa-Clara-University@2x.png 1.5x" alt="Santa Clara University" width="572" height="28" />
+                <img src="css/assets/header-small-the-jesuit-university-in-silicon-valley.png" srcset="css/assets/header-small-the-jesuit-university-in-silicon-valley@2x.png 1.5x" alt="The Jesuit University In Silicon Valley" width="433" height="20" /></a>
+            </div>
+            <div class="col-xs-12 col-sm-3 clearfix site-header-search-and-toggles">
+              <button type="button" class="visible-xs-block menu-toggle menu-toggle-main" data-toggle="offcanvas" data-target="#scu-main-navigation">
+                <span class="caret left-caret"></span> <span class="menu-label"><span class="sr-only">Toggle navigation</span> Menu</span>
+              </button>
+              <button type="button" class="visible-xs-block menu-toggle menu-toggle-gateways" data-toggle="offcanvas" data-target="#scu-gateway-navigation">
+                <span class="menu-label text-hide"><span class="sr-only">Toggle navigation</span> Gateways</span> <span class="caret right-caret"></span>
+              </button>
+              <form class="pull-right site-header-search">
+                <div class="input-group">
+                  <input type="search" class="form-control" placeholder="SEARCH">
+                  <span class="input-group-btn">
+                    <button class="btn btn-default" type="button" aria-label="Submit"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
+                  </span>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <nav class="main-navigation core-navigation offcanvas-left" id="scu-main-navigation">
+        <div class="container sidebar-offcanvas">
+          <ul class="nav nav-pills">
+            <li><a href="#">About SCU</a></li>
+            <li><a href="#">Academics</a></li>
+            <li><span class="currentbranch0"><a href="#">Admission</a></span></li>
+            <li><a href="#">Campus Life</a></li>
+            <li><a href="#">Athletics</a></li>
+            <li><a href="#">Global</a></li>
+            <li><a href="#">Giving</a></li>
+            <li><a href="#">News & Events</a></li>
+          </ul>
+          </div>
+        </div>
+      </nav>
+
+      <nav class="gateway-navigation core-gateways offcanvas-right" id="scu-gateway-navigation">
+        <div class="container sidebar-offcanvas">
+          <ul class="nav nav-pills navbar-right">
+            <li><a href="#">Students</a></li>
+            <li><a href="#">Faculty & Staff</a></li>
+            <li><a href="#">Families</a></li>
+            <li><a href="#">Alumni</a></li>
+            <li><a href="#">Visitors</a></li>
+            <li class="slide-menu"><a href="#" role="button" aria-expanded="false"><span class="gateway-dropdown">Offices & Services <span class="caret"></span></span></a>
+              <div class="slide-panel">
+                <div class="container">
+                  <div class="row">
+                    <div class="col-sm-6">
+                      <h6>Offices of SCU</h6>
+                      <div class="row">
+                        <div class="col-sm-6">
+                          <ul role="menu">
+                            <li><a href="#">Education and Counseling Psychology</a></li>
+                            <li><a href="#">School of Engineering</a></li>
+                          </ul>
+                        </div>
+                        <div class="col-sm-6">
+                          <ul role="menu">
+                            <li><a href="#">Jesuit School of Theology</a></li>
+                            <li><a href="#" class="more-link">All Offices of SCU &raquo;</a></li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-sm-6">
+                      <h6>Services at SCU</h6>
+                      <div class="row">
+                        <div class="col-sm-6">
+                          <ul>
+                            <li><a href="#">An SCU Service Link</a></li>
+                            <li><a href="#">Another Service of SCU</a></li>
+                          </ul>
+                        </div>
+                        <div class="col-sm-6">
+                          <ul>
+                            <li><a href="#">Ignatian Center for Jesuit Education</a></li>
+                            <li><a href="#" class="more-link">All Services at SCU &raquo;</a></li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li class="slide-menu"><a href="#" role="button" aria-expanded="false"><span class="gateway-dropdown">Centers & Schools <span class="caret"></span></span></a>
+              <div class="slide-panel">
+                <div class="container">
+                  <div class="row">
+                    <div class="col-sm-6">
+                      <h6>Schools &amp; Colleges</h6>
+                      <div class="row">
+                        <div class="col-sm-6">
+                          <ul role="menu">
+                            <li><a href="#">College of Arts & Sciences</a></li>
+                            <li><a href="#">Education and Counseling Psychology</a></li>
+                            <li><a href="#">Leavey School of Business</a></li>
+                            <li><a href="#">School of Engineering</a></li>
+                          </ul>
+                        </div>
+                        <div class="col-sm-6">
+                          <ul role="menu">
+                            <li><a href="#">Jesuit School of Theology</a></li>
+                            <li><a href="#">School of Law</a></li>
+                            <li><a href="#">Summer School</a></li>
+                            <li><a href="#" class="more-link">All Schools & Colleges &raquo;</a></li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-sm-6">
+                      <h6>Centers of Distinction</h6>
+                      <div class="row">
+                        <div class="col-sm-6">
+                          <ul>
+                            <li><a href="#">Center for Science, Technology, and Society</a></li>
+                            <li><a href="#">Ignatian Center for Jesuit Education</a></li>
+                            <li><a href="#">Markkula Center for Applied Ethics</a></li>
+                          </ul>
+                        </div>
+                        <div class="col-sm-6">
+                          <ul>
+                            <li><a href="#">A sample center link</a></li>
+                            <li><a href="#">A center link of distinction</a></li>
+                            <li><a href="#">Another Center Yonder</a></li>
+                            <li><a href="#" class="more-link">All Centers & Institutes &raquo;</a></li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div class="slide-menu-backdrop"></div>
+      </nav>
+    
+    </header>
