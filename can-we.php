70dbb9ec28b9f7248e7aeff53464224cca6f6363
jasonpontius <jason@whitewhale.net>
2015-09-01 13:41:44 -0700

    Vertical "can we" option
    Add class=“vertical” to any “can-we feature module” to apply vertical
    style (seen here:
    http://scu.livewhale.net/design/layout/#83-full-width.jpg)

--- can-we.php
+++ can-we.php
@@ -14,7 +14,7 @@
   <body> 
 	<?php require('includes/header.php'); ?>
 
-    <div class="can-we feature module" style="background-image:url(_examples/ghana.jpg); background-position:50% 10%;">
+    <div class="can-we feature module " style="background-image:url(_examples/ghana.jpg); background-position:50% 10%;">
       <div class="container can-we-body">
 	    <div class="can-we-headline">
 		    <span>Engineering the Future</span>
@@ -66,138 +66,63 @@
         <p>PHOTO: SCU's Mobile Mission Control lab can be found scanning the heavens from various places on campus.</p>
       </div>
     </div>
-      <div class="container" id="content">
-        
 
-      <div class="row">
-        <div class="col-sm-8">
-          <h1 class="page-title">"Can We" Features</h1>
 
-          <ol class="breadcrumb">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Section Title</a></li>
-            <li><a href="#">Parent Page</a></li>
-            <li class="active">"Can We" Features</li>
-          </ol>
-
-          <div class="intro">
-            <p>"Can We" modules go all across the page.</p>
+    <div class="can-we feature module vertical" style="background-image:url(_examples/ghana.jpg); background-position:50% 10%;">
+      <div class="container can-we-body">
+	    <div class="can-we-headline">
+		    <span>Engineering the Future</span>
+		    <h2>Spring Break in Ghana</h2>
+	    </div>
+        <div class="row can-we-boxes">
+          <div class="col-xs-4">
+	          <div class="can-we-box">
+	            <h2><span>Can we?</span></h2>
+	            <div class="can-we-photo">
+		            <img src="_examples/chris-kitts.jpg"/>
+	            </div>
+	            <p>We’re helping local teams on the ground track river erosion using solar-powered drones.</p>
+	            <div class="can-we-credit">
+	                Chris Kitts<br/>
+	                Robotics & Engineering
+	            </div>
+	          </div>
           </div>
-          
-          <p>The "Can We"  module is primarily used on the main institutional homepage, but can be used on the homepages of major academic units as well.</p>
-          <blockquote>
-            <code>
-&lt;div class="can-we feature module" style="background-image:url(_examples/ghana.jpg); background-position:50% 10%;"&gt;<br/>
-&nbsp;&nbsp;&lt;div class="container can-we-body"&gt;<br/>
-&nbsp;&nbsp;&lt;div class="can-we-headline"&gt;<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;&lt;span&gt;Engineering the Future&lt;/span&gt;<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;&lt;h2&gt;Spring Break in Ghana&lt;/h2&gt;<br/>
-&nbsp;&nbsp;&lt;/div&gt;<br/>
-&nbsp;&nbsp;&lt;div class="row can-we-boxes"&gt;<br/>
-&nbsp;&nbsp;  &lt;div class="col-xs-4"&gt;<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;  &lt;div class="can-we-box"&gt;<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;h2&gt;&lt;span&gt;Can we?&lt;/span&gt;&lt;/h2&gt;<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;div class="can-we-photo"&gt;<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;img src="_examples/chris-kitts.jpg"/&gt;<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;/div&gt;<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;p&gt;We’re helping local teams on the ground track river erosion using solar-powered drones.&lt;/p&gt;<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;div class="can-we-credit"&gt;<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Chris Kitts&lt;br/&gt;<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Robotics & Engineering<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;/div&gt;<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;  &lt;/div&gt;<br/>
-&nbsp;&nbsp;  &lt;/div&gt;<br/>
-&nbsp;&nbsp;  &lt;div class="col-xs-4"&gt;<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;  &lt;div class="can-we-box"&gt;<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;h2&gt;&lt;span&gt;Should we?&lt;/span&gt;&lt;/h2&gt;<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;div class="can-we-photo"&gt;<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;img src="_examples/chris-kitts.jpg"/&gt;<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;/div&gt;<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;p&gt;This is a shorter sentence.&lt;/p&gt;<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;div class="can-we-credit"&gt;<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Chris Kitts&lt;br/&gt;<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Robotics & Engineering<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;/div&gt;<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;  &lt;/div&gt;<br/>
-&nbsp;&nbsp;  &lt;/div&gt;<br/>
-&nbsp;&nbsp;  &lt;div class="col-xs-4"&gt;<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;  &lt;div class="can-we-box"&gt;<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;h2&gt;&lt;span&gt;Will we?&lt;/span&gt;&lt;/h2&gt;<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;div class="can-we-photo"&gt;<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;img src="_examples/chris-kitts.jpg"/&gt;<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;/div&gt;<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;p&gt;We’re helping local teams on the ground track river erosion using solar-powered drones.  This is too long.&lt;/p&gt;<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;div class="can-we-credit"&gt;<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Chris Kitts&lt;br/&gt;<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Robotics & Engineering<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;/div&gt;<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;  &lt;/div&gt;<br/>
-&nbsp;&nbsp;  &lt;/div&gt;<br/>
-&nbsp;&nbsp;&lt;/div&gt;<br/>
-  &lt;/div&gt;&nbsp;&nbsp; <br/>
-  &lt;div class="carousel-caption container"&gt;<br/>
-&nbsp;&nbsp;&lt;p&gt;PHOTO: SCU's Mobile Mission Control lab can be found scanning the heavens from various places on campus.&lt;/p&gt;<br/>
-  &lt;/div&gt;<br/>
-&lt;/div&gt;
-            </code>
-          </blockquote>
-          <ul>
-            <li>We're using <code>div.can-we.feature.module</code> as the full-width container with a CSS background-image with an inline <code>style</code> attribute. Currently, the background image is centered as the image stretches as the browser resizes, but that point of focus can be adjusted if needed for individual images using CSS.</li>
-            <li><code>div.container</code> (from Bootstrap) constrains the "stage" of copy.</li>
-            <li>The primary headline ("Spring Break in Ghana" above) is in white by default, but can be switched to black by adding the class <code>.reverse</code> to <code>div.can-we-headline</code>.
-          </ul>
+          <div class="col-xs-4">
+	          <div class="can-we-box">
+	            <h2><span>Should we?</span></h2>
+	            <div class="can-we-photo">
+		            <img src="_examples/chris-kitts.jpg"/>
+	            </div>
+	            <p>This is a shorter sentence.</p>
+	            <div class="can-we-credit">
+	                Chris Kitts<br/>
+	                Robotics & Engineering
+	            </div>
+	          </div>
+          </div>
+          <div class="col-xs-4">
+	          <div class="can-we-box">
+	            <h2><span>Will we?</span></h2>
+	            <div class="can-we-photo">
+		            <img src="_examples/chris-kitts.jpg"/>
+	            </div>
+	            <p>We’re helping local teams on the ground track river erosion using solar-powered drones.  This is too long.</p>
+	            <div class="can-we-credit">
+	                Chris Kitts<br/>
+	                Robotics & Engineering
+	            </div>
+	          </div>
+          </div>
         </div>
-        <div class="col-sm-4">
-                    <nav class="side-navigation match-page-title">
-            <a class="side-navigation-header" href="index.php">SCU Templates and Modules</a>
-            <ul>
-              <li><a href="homepage.php">Homepage</a></li>
-              <li><a href="inside-two-columns.php">Inside Page: 2 Columns</a></li>
-              <li><a href="inside-three-columns.php">Inside Page: 3 Columns</a></li>
-              <li><a href="inside-full-width.php">Inside Page: Full Width</a></li>
-              <li><a href="inside-grid.php">Inside Page: Grid</a></li>
-              <li><a href="inside-jumbotron-three-columns.php">Inside Page: Jumbotron Three Columns</a></li>
-              <li><a href="inside-jumbotron-two-columns.php">Inside Page: Jumbotron Two Columns</a></li>
-              <li>
-                <a href="one-column-modules.php">One Column Modules</a>
-                <ul class="multilevel-linkul-0">
-                  <li><a href="building-one-column-modules.php">Building One Column Modules</a></li>
-                  <li><a href="#">A test link</a></li>
-                </ul>
-              </li>
-              <li>
-                <a href="two-column-modules.php">Two Column Modules</a>
-              </li>
-              <li>
-                <a href="full-width-modules.php">Full Width Modules</a>
-                <ul class="multilevel-linkul-0">
-                  <li><a href="can-we.php">Hero Features</a></li>
-                  <li><a href="hero-features.php">Hero Features</a></li>
-                  <li><a href="landing-hero-features.php">Landing Hero Features</a></li>
-                  <li><a href="subheaders.php">Subheaders</a></li>
-                </ul>
-              </li>
-              <li class="active">
-                <a href="headers.php">Headers</a>
-                <ul class="multilevel-linkul-0">
-                  <li><a href="header-core-nav-core-gateways.php">Core Nav, Core Gateways</a></li>
-                  <li class="active"><a href="header-site-nav-site-gateways.php">Site Nav, Site Gateways</a>
-                    <ul class="multilevel-linkul-1">
-                      <li><a href="header-stone-site-nav.php">Example: Stone Nav</a></li>
-                      <li class="active"><a href="header-gray-site-nav.php">Example: Gray Nav</a></li>
-                      <li><a href="header-black-site-nav.php">Example: Black Nav</a></li>
-                    </ul>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </nav>
-
-          
-        </div>
+      </div>     
+      <div class="carousel-caption container">
+        <p>PHOTO: SCU's Mobile Mission Control lab can be found scanning the heavens from various places on campus.</p>
       </div>
-
-      </div>
+    </div>
+    
+    
+    
       
 	<?php require('includes/footer.php'); ?>
   </body>
