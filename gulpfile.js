761bcddb2871fe131401f1cd77d0f6a90008dd51
Nic Bertino <nbertino@scu.edu>
2015-09-16 17:00:39 -0700

    fixed package.json to allow npm install to work

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,27 @@
 var gulp = require('gulp');
+var less = require('gulp-less');
+var path = require('path');
+var jekyll = require('gulp-jekyll');
 
 gulp.task('default', function() {
     console.log("I'm not doing anything but I look cool");
+});
+gulp.task('less', function () {
+    return gulp.src('./less/**/*.less')
+        .pipe(less({
+            paths: [ path.join(__dirname, 'less', 'includes') ]
+        }))
+        .pipe(gulp.dest('./public/css'));
+});
+gulp.task('docs', function() {
+    gulp.src(['./index.html', './_layouts/*.html', './_posts/*.{markdown,md}'])
+        .pipe(jekyll({
+            source: './',
+            destination: './deploy/',
+            bundleExec: true
+        }))
+        .pipe(gulp.dest('./deploy/'));
+});
+gulp.task('deploy', function() {
+
 });
