79380e8e6ac8af28489729efc40720f00dc19467
jasonpontius <jason@whitewhale.net>
2015-09-24 13:40:50 -0700

    One-column profiles now float left when inside page body
    As seen in Issue #5.
    
    http://scu.babywhale.net/person-spotlight.php
    
    Question: this issue refers to “Profile and Link List.” Is this for
    more than the reported content type? If not, this issue can probably be
    closed.

---
+++ person-spotlight.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SCU | SCU Templates and Modules | Interior Page, Two Columns</title>
+
+	<?php require('includes/head.php'); ?>
+  </head>
+  <body>
+	<?php require('includes/header.php'); ?>
+      
+      <div class="container" id="content">
+        
+      <div class="row">
+        <div class="col-sm-8">
+          
+              <div class="one-column person-spotlight module">
+                <ul class="media-list">
+                  <li class="media">
+                    <div class="media-left">
+                      <a href="#">
+                        <img class="media-object img-circle img-thumbnail" src="_examples/Mourad2p.jpg" width="141" height="141" alt="">
+                      </a>
+                    </div>
+                    <div class="media-body">
+                      <h3 class="person-spotlight-department">Electrical Engineering</h3>
+                      <h4 class="media-heading"><a href="#">Person A</a></h4>
+                      <p>encourages regular meetings among the students she advises.</p>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              <div class="one-column person-spotlight module">
+                <ul class="media-list">
+                  <li class="media">
+                    <div class="media-left">
+                      <a href="#">
+                        <img class="media-object img-circle img-thumbnail" src="_examples/Mourad2p.jpg" width="141" height="141" alt="">
+                      </a>
+                    </div>
+                    <div class="media-body">
+                      <h3 class="person-spotlight-department">Electrical Engineering</h3>
+                      <h4 class="media-heading"><a href="#">Person B</a></h4>
+                      <p>encourages regular meetings among the students she advises.</p>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              <div class="one-column person-spotlight module">
+                <ul class="media-list">
+                  <li class="media">
+                    <div class="media-left">
+                      <a href="#">
+                        <img class="media-object img-circle img-thumbnail" src="_examples/Mourad2p.jpg" width="141" height="141" alt="">
+                      </a>
+                    </div>
+                    <div class="media-body">
+                      <h3 class="person-spotlight-department">Electrical Engineering</h3>
+                      <h4 class="media-heading"><a href="#">Person C</a></h4>
+                      <p>encourages regular meetings among the students she advises.</p>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              <div class="one-column person-spotlight module">
+                <ul class="media-list">
+                  <li class="media">
+                    <div class="media-left">
+                      <a href="#">
+                        <img class="media-object img-circle img-thumbnail" src="_examples/Mourad2p.jpg" width="141" height="141" alt="">
+                      </a>
+                    </div>
+                    <div class="media-body">
+                      <h3 class="person-spotlight-department">Electrical Engineering</h3>
+                      <h4 class="media-heading"><a href="#">Person D</a></h4>
+                      <p>encourages regular meetings among the students she advises.</p>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+
+
+        </div>
+      </div>
+
+      </div>
+      
+	<?php require('includes/footer.php'); ?>
+  </body>
+</html>
