af962ff73a94a8ec32ba291be56f5f92ef632585
jasonpontius <jason@whitewhale.net>
2015-10-02 14:52:12 -0700

    Person spotlight changes
    These changes were made a while back and hadn’t been committed.  Please
    review behavior of multiple spotlights and make sure things still
    look/work great.

--- person-spotlight.php
+++ person-spotlight.php
@@ -19,6 +19,58 @@
       <div class="row">
         <div class="col-sm-8">
           
+              <div class="one-column person-spotlight module grid">
+                <ul class="media-list">
+                  <li class="media">
+                    <div class="media-left">
+                      <a href="#">
+                        <img class="media-object img-circle img-thumbnail" src="_examples/Mourad2p.jpg" width="141" height="141" alt="">
+                      </a>
+                    </div>
+                    <div class="media-body">
+                      <h3 class="person-spotlight-department">Electrical Engineering</h3>
+                      <h4 class="media-heading"><a href="#">Person A</a></h4>
+                      <p>encourages regular meetings among the students she advises.</p>
+                    </div>
+                  </li>
+                  <li class="media">
+                    <div class="media-left">
+                      <a href="#">
+                        <img class="media-object img-circle img-thumbnail" src="_examples/Mourad2p.jpg" width="141" height="141" alt="">
+                      </a>
+                    </div>
+                    <div class="media-body">
+                      <h3 class="person-spotlight-department">Electrical Engineering</h3>
+                      <h4 class="media-heading"><a href="#">Person B</a></h4>
+                      <p>encourages regular meetings among the students she advises.</p>
+                    </div>
+                  </li>
+                   <li class="media">
+                    <div class="media-left">
+                      <a href="#">
+                        <img class="media-object img-circle img-thumbnail" src="_examples/Mourad2p.jpg" width="141" height="141" alt="">
+                      </a>
+                    </div>
+                    <div class="media-body">
+                      <h3 class="person-spotlight-department">Electrical Engineering</h3>
+                      <h4 class="media-heading"><a href="#">Person C</a></h4>
+                      <p>encourages regular meetings among the students she advises.</p>
+                    </div>
+                  </li>
+                   <li class="media">
+                    <div class="media-left">
+                      <a href="#">
+                        <img class="media-object img-circle img-thumbnail" src="_examples/Mourad2p.jpg" width="141" height="141" alt="">
+                      </a>
+                    </div>
+                    <div class="media-body">
+                      <h3 class="person-spotlight-department">Electrical Engineering</h3>
+                      <h4 class="media-heading"><a href="#">Person D</a></h4>
+                      <p>encourages regular meetings among the students she advises.</p>
+                    </div>
+                  </li>
+            </ul>
+              </div>
               <div class="one-column person-spotlight module">
                 <ul class="media-list">
                   <li class="media">
@@ -33,10 +85,6 @@
                       <p>encourages regular meetings among the students she advises.</p>
                     </div>
                   </li>
-                </ul>
-              </div>
-              <div class="one-column person-spotlight module">
-                <ul class="media-list">
                   <li class="media">
                     <div class="media-left">
                       <a href="#">
@@ -49,11 +97,7 @@
                       <p>encourages regular meetings among the students she advises.</p>
                     </div>
                   </li>
-                </ul>
-              </div>
-              <div class="one-column person-spotlight module">
-                <ul class="media-list">
-                  <li class="media">
+                   <li class="media">
                     <div class="media-left">
                       <a href="#">
                         <img class="media-object img-circle img-thumbnail" src="_examples/Mourad2p.jpg" width="141" height="141" alt="">
@@ -65,11 +109,7 @@
                       <p>encourages regular meetings among the students she advises.</p>
                     </div>
                   </li>
-                </ul>
-              </div>
-              <div class="one-column person-spotlight module">
-                <ul class="media-list">
-                  <li class="media">
+                   <li class="media">
                     <div class="media-left">
                       <a href="#">
                         <img class="media-object img-circle img-thumbnail" src="_examples/Mourad2p.jpg" width="141" height="141" alt="">
@@ -81,7 +121,7 @@
                       <p>encourages regular meetings among the students she advises.</p>
                     </div>
                   </li>
-                </ul>
+            </ul>
               </div>
 
 
