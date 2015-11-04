64308494f76a3b9b4257bd9161e39aea6b0cbcc1
jasonpontius <jason@whitewhale.net>
2015-11-03 16:14:53 -0800

    Changes to header spacing and links
    It doesn’t make sense to separate Offices from Services in the global
    nav.  I know Jason S. typed it up that way, but that wasn’t ever our
    intent; O&S is a global category that applies to a wide variety of
    sites. No need to sweat over which is which.   This commit makes them
    into a single list, in four columns, with a link to more.  (We feel
    that this list could comfortably grow, even double, in length if people
    want to fight about being there.)
    
    Also changed the Schools nav to be 7/5 rather than 6/6, so the colleges
    will form a tighter set.

--- includes/header.php
+++ includes/header.php
@@ -87,85 +87,87 @@
             <li class="slide-menu"><a href="#" role="button" aria-expanded="false"><span class="gateway-dropdown">Offices & Services <span class="caret"></span></span></a>
               <div class="slide-panel">
                 <div class="container">
-                  <div class="row">
-                    <div class="col-sm-6">
-                      <h6>Offices of SCU</h6>
-                      <div class="row">
-                        <div class="col-sm-6">
-                          <ul role="menu">
-                            <li><a href="#">Education and Counseling Psychology</a></li>
-                            <li><a href="#">School of Engineering</a></li>
-                          </ul>
-                        </div>
-                        <div class="col-sm-6">
-                          <ul role="menu">
-                            <li><a href="#">Jesuit School of Theology</a></li>
-                            <li><a href="#" class="more-link">All Offices of SCU &raquo;</a></li>
-                          </ul>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-sm-6">
-                      <h6>Services at SCU</h6>
-                      <div class="row">
-                        <div class="col-sm-6">
-                          <ul>
-                            <li><a href="#">An SCU Service Link</a></li>
-                            <li><a href="#">Another Service of SCU</a></li>
-                          </ul>
-                        </div>
-                        <div class="col-sm-6">
-                          <ul>
-                            <li><a href="#">Ignatian Center for Jesuit Education</a></li>
-                            <li><a href="#" class="more-link">All Services at SCU &raquo;</a></li>
-                          </ul>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+									<div class="row">
+						        <div class="col-sm-12">
+							        <h6>Offices and Services at Santa Clara University</h6>
+						        </div>
+				            <div class="col-sm-3">
+				              <ul>
+												<li><a class="" href="/enrollment-management/">Enrollment Management</a></li>
+												<li><a class="" href="/cm/">Campus Ministry</a></li>
+												<li><a class="" href="/university-operations/campus-safety/">Campus Safety</a></li>
+												<li><a class="" href="/university-operations/facilities/">Facilities</a></li>
+												</ul>
+				            </div>
+				            <div class="col-sm-3">
+				              <ul>
+												<li><a class="" href="/diversity/">Diversity &amp; Inclusion</a></li>
+												<li><a class="" href="/is/">Information&nbsp;Services</a></li>
+												<li><a href="/campus-recreation/">Recreation</a></li>
+												<li><a class="" href="/sustainability/">Sustainability</a></li>
+												</ul>
+				              
+				            </div>
+				          	<div class="col-sm-3">
+				            	<ul>
+												<li>Arts at SCU</li>
+												<li>Bookstore</li>
+												<li><a class="" href="/career-center/">Career Center</a></li>
+												<li><a class="" href="/hr/">Human Resources</a></li>
+												</ul>
+				            </div>
+				            <div class="col-sm-3">
+				              <ul>
+												<li><a class="" href="/library/">Library</a></li>
+												<li><a class="" href="/missionchurch/">Mission Church</a></li>
+												<li><a href="/login/">SCU Login</a></li>
+												</ul>
+				              <a href="/portals/all-offices-and-services/" class="more-link">All Offices and Services »</a>
+				            </div>
+				        
+						      </div>
+          			</div>
               </div>
             </li>
             <li class="slide-menu"><a href="#" role="button" aria-expanded="false"><span class="gateway-dropdown">Centers & Schools <span class="caret"></span></span></a>
               <div class="slide-panel">
                 <div class="container">
-<div class="row">
-        
-        <div class="col-sm-8">
-          <h6>Schools and Colleges</h6>
-          <div class="row">
-          	<div class="col-sm-6">
-              <ul>
-<li><a class="" href="/cas/">College of Arts &amp; Sciences</a></li>
-<li><a class="" href="/ecp/">Education and Counseling Psychology</a></li>
-<li><a class="" href="/business/">Leavey School of Business</a></li>
-</ul>
-          	</div>
-          	<div class="col-sm-6">
-              <ul>
-<li><a class="" href="/engineering/">School of Engineering</a></li>
-<li><a href="/jst/">Jesuit School of Theology</a></li>
-<li><a href="http://law.scu.edu">School of Law</a></li>
-</ul>
-              
-             </div>
-          </div>
-        </div>
-        
-        <div class="col-sm-4">
-          <h6> Centers of Distinction</h6>
-          <div class="row">
-          	<div class="col-sm-12">
-            	<ul>
-<li><a href="http://www.scu-social-entrepreneurship.org/">Miller Center for Social Entrepreneurship</a></li>
-<li><a class="" href="/ignatiancenter/">Ignatian Center for Jesuit Education</a></li>
-<li><a class="" href="/ethics/">Markkula Center for Applied Ethics</a></li>
-</ul><a href="/portals/centers-and-institutes/" class="more-link">Centers and Institutes »</a>
-            </div>
-          </div>
-        </div>
-        
-      </div>                </div>
+										<div class="row">
+							        <div class="col-sm-7">
+							          <h6>Schools and Colleges</h6>
+							          <div class="row">
+							          	<div class="col-sm-6">
+							              <ul>
+															<li><a class="" href="/cas/">College of Arts &amp; Sciences</a></li>
+															<li><a class="" href="/ecp/">Education and Counseling Psychology</a></li>
+															<li><a class="" href="/business/">Leavey School of Business</a></li>
+															</ul>
+									          	</div>
+									          	<div class="col-sm-6">
+													 <ul>
+															<li><a class="" href="/engineering/">School of Engineering</a></li>
+															<li><a href="/jst/">Jesuit School of Theology</a></li>
+															<li><a href="http://law.scu.edu">School of Law</a></li>
+														</ul>
+							             </div>
+							          </div>
+							        </div>
+							        
+							        <div class="col-sm-5">
+							          <h6> Centers of Distinction</h6>
+							          <div class="row">
+							          	<div class="col-sm-12">
+							            	<ul>
+							<li><a href="http://www.scu-social-entrepreneurship.org/">Miller Center for Social Entrepreneurship</a></li>
+							<li><a class="" href="/ignatiancenter/">Ignatian Center for Jesuit Education</a></li>
+							<li><a class="" href="/ethics/">Markkula Center for Applied Ethics</a></li>
+							</ul><a href="/portals/centers-and-institutes/" class="more-link">Centers and Institutes »</a>
+							            </div>
+							          </div>
+							        </div>
+							        
+							      </div> 
+                </div>
               </div>
             </li>
           </ul>
