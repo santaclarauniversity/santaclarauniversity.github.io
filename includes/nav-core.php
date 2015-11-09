41fe3ca9a672bc25732c45a9aee6310d77791702
jasonpontius <jason@whitewhale.net>
2015-11-09 12:24:24 -0800

    Revert "First set of header designs"
    This reverts commit 590c8021de55bf3a7396b473cff33bebea0af3c7.

--- includes/nav-core.php
+++
@@ -1,5 +0,0 @@
-<nav class="main-navigation core-navigation offcanvas-left affix-top" id="scu-main-navigation">
-  <div class="container sidebar-offcanvas">
-    <ul class="nav nav-pills"><li><a href="/about-scu/">About SCU</a></li><li class="active"><a href="/academics/">Academics</a></li><li><a href="/admission/">Admission</a></li><li><a href="/campus-life/">Campus Life</a></li><li><a href="/athletics/">Athletics</a></li><li><a href="/global/">Global</a></li><li><a href="/giving/">Giving</a></li><li><a href="/news-and-events/">News &amp; Events</a></li></ul>
-  </div>
-</nav>
