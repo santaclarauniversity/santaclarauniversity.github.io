779101498478708eff0798e4af924159c1c01430
jasonpontius <jason@whitewhale.net>
2015-12-08 15:54:51 -0800

    New headers for minors and certificates

---
+++ headers/cas/latinamericanstudies/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+    <title>Latin American Studies - College of Arts and Sciences - Santa Clara University</title>
+  
+	<?php require('../../../includes/head.php'); ?>
+	
+  </head>  <body>
+	<a class="sr-only sr-only-focusable" href="#content">Skip to main content</a>
+    <header class="site-header uses-core-navigation site-header-with-seal">
+      <?php require('../../../includes/header-cas.php'); ?>
+      <?php require('../../../includes/nav-institutional.php'); ?>
+    </header>
+    <div class="subheader module" style="background-image:url('/headers/_assets/images/latinamericanstudies.jpg');">
+      <div class="container">
+        <div class="row">
+    		  <div class="col-sm-8">
+    			    <h1>
+    			      <span>Minor in </span>Latin American Studies
+    			    </h1>
+    		  </div>
+          <div class="col-sm-4">
+          <?php require('../../../includes/department-switcher.php'); ?>
+  		  </div>
+        </div>
+      </div>
+    </div>
+      <?php require('../../../includes/footer.php'); ?>
+  </body>
+</html>
