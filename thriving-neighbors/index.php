794add4edd2c50d681cb53ec452ee71671276585
jasonpontius <jason@whitewhale.net>
2015-12-11 14:56:55 -0800

    New work for Thriving Neighbors Initiative
    Several images and two HTML files for design layouts we did for TNI.
    The only new code elements introduced here are intended to be globally
    available:
    
    1. a new “Project Title” element (div.project-title) that functions as
    a subheader (its code is included in subheaders.less) but is suited for
    projects within centers;
    2. adding a title and view all element to the gallery module. (This
    code was committed earlier; in this commit the TNI main page has a
    gallery with the new elements.)

---
+++ thriving-neighbors/index.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Thriving Neighbors Initiative - Ignatian Center for Jesuit Education</title>
+
+	<?php require('../includes/head.php'); ?>
+  </head>
+  <body class="black-nav centers tall-header">
+    <a class="sr-only sr-only-focusable" href="#content">Skip to main content</a>
+   <header class="site-header site-header-custom-title">
+      <?php require('../includes/header-ignatian-center.php'); ?>
+      <?php require('../includes/nav-ignatian-center.php'); ?>
+    </header>
+    <div class="hero module" style="background-image:url(lead-photo.jpg);">
+      <div class="container">
+        <div class="hero-body">
+          <div class="row">
+            <div class="col-xs-6 col-xs-push-6 col-sm-4 col-sm-push-8">
+              <div class="caption">
+                <p>About this photo: (Photo description.)</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+    
+    <div class="project-title">
+      <div class="container">
+				<h1>Thriving Neighbors</h1>
+      </div>      
+    </div>
+    
+      <div class="container" id="content">
+        
+      <div class="row">
+        <div class="col-sm-8">
+
+          <div class="intro">
+            <p>The Thriving Neighbors Initiative builds connections between Santa Clara University and its surrounding neighborhood, the Greater Washington Community of San Jose.  These connections benefit both Greater Washington residents and SCU students.</p>
+          </div>
+          
+          <div class="row">
+            <div class="col-md-6">
+              <div class="one-column featured-story module">
+                <a href="#" class="thumbnail"><img src="anthony-hascheff.jpg" alt=""></a>
+                <h4 class="featured-story-heading"><a href="#">Serving Locally, Serving Globally</a></h4>
+                <p>Entering SCU as a baseball player and finance major, Anthony Haschett didn't expect his Santa Clara education to lead him to the Peace Corps. But the experiences he had at SCU— including work as a Neighborhood Prosperity Initiative intern in the Washington neighborhood— opened his eyes to a new path.</p>
+              </div>              
+            </div>
+            <div class="col-md-6">
+             <div class="one-column featured-story module">
+                <a href="#" class="thumbnail"><img src="stephanie-molina.jpg" alt=""></a>
+                <h4 class="featured-story-heading"><a href="#">Finding Her Voice: Stephanie Molina</a></h4>
+                <p>Stephanie didn’t like to speak up in class. She's a bright kid who does well in school, but she typically preferred to keep to herself in her early years at Washington Elementary.  So it was a surprise when she spoke up one day and said she intended to go to Santa Clara and become an engineer. </p>
+              </div>
+            </div>
+          </div>
+          
+          
+						          
+						<div class="two-column gallery module">
+							<div class="row">
+								<div class="col-sm-12 gallery-title">
+									<h3>Our Programs</h3>
+								</div>
+								<div class="col-sm-9 full-image">
+									<div class="gallery-image">
+											<a href="#" class="thumbnail"><img src="Madres.jpg" alt="Madres"></a>
+											<div class="gallery-caption">
+												<p><a href="#"><strong>Madres Walking to Health:</strong></a>This grant project develops a sustainable community health program for the parents and their children at Washington Elementary School.</p>
+											</div>
+										</div>
+								</div>
+								<div class="col-sm-3">
+									<div class="slides">
+										<div class="gallery-image">
+											<a href="#" class="thumbnail"><img src="Abriendo-Puertas.jpg" alt="Abriendo-Puertas" ></a>
+											<div class="gallery-caption">
+												<p><a href="#"><strong>Abriendo Puertas: Opening Doors to Early Childhood Education Via iPads:</strong></a> This grant project works with five parents in the Greater Washington community of San Jose to support math and literacy development among children between the ages of two and five.</p>
+											</div>
+										</div>
+										<div class="gallery-image">
+											<a href="#" class="thumbnail"><img src="Adult-Education-Center.jpg" alt="Adult-Education-Center" ></a>
+											<div class="gallery-caption">
+												<p><a href="#"><strong>Adult Education Center/Centro Para Educacion de Adultos:</strong></a> The grant project provides a place for ongoing discussions and reflections with a focus issues of identity, culture, faith, and community.</p>
+											</div>
+										</div>
+										<div class="gallery-image">
+											<a href="#" class="thumbnail"><img src="Resilient-Families-Project.JPG" alt="Resilient-Families-Project" width="225" height="169"></a>
+											<div class="gallery-caption">
+											<p><a href="#"><strong>Resilient Families Project:</strong></a> This grant project provides a series of workshops for families in the Greater Washington neighborhood that will support parents in forming stronger relationships with their children means to combat gang pressures.</p>
+											</div>
+										</div>
+										<div class="gallery-image">
+											<a href="#" class="thumbnail"><img src="Madres.jpg" alt="Madres" ></a>
+											<div class="gallery-caption">
+												<p><a href="#"><strong>Madres Walking to Health:</strong></a>This grant project develops a sustainable community health program for the parents and their children at Washington Elementary School.</p>
+											</div>
+										</div>
+									</div>
+									<a href="#" class="gallery-viewall thumbnail">View All »</a>
+
+								</div>
+							
+							</div>
+						</div>
+          
+        </div>
+        <div class="col-sm-4">
+          <nav class="side-navigation match-page-title">
+						<ul>
+							<li><a href="#">About</a>
+								<ul>
+									<li><a href="#">Vision</a></li>
+									<li><a href="#">Strategies</a></li>
+									<li><a href="#">Impact</a></li>
+									<li><a href="#">History</a></li>
+								</ul>
+							</li>
+							<li><a href="#">The Community</a></li>
+							<li><a href="#">Programs</a>
+								<ul>
+									<li><a href="#">Program Archive</a></li>
+								</ul>
+							</li>
+							<li><a href="#">Faculty Project Funding</a></li>
+							<li><a href="#">Get Involved</a>
+								<ul>
+									<li><a href="#">Support Us</a></li>
+									<li><a href="#">Volunteer</a></li>
+									<li><a href="#">Partner Agencies</a></li>
+									<li><a href="#">Contact Us</a></li>
+								</ul>
+							</li>
+							<li><a href="#">News &amp; Events</a>
+								<ul>
+									<li><a href="#">News Archive</a></li>
+								</ul>
+							</li>
+						</ul>            
+          </nav>
+          <div class="one-column video module">
+            <div class="embed-responsive embed-responsive-16by9">
+              <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/XiEUKGmKmZI" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+            </div>
+            <p class="video-caption">Video: <a href="https://www.youtube.com/watch?v=XiEUKGmKmZI">TNI Academic Enrichment Program</a></p>
+          </div>
+
+
+        </div>
+      </div>
+
+      </div>
+ 	<?php require('../includes/footer.php'); ?>
+  </body>
+</html>
---
+++ thriving-neighbors/program.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Thriving Neighbors Initiative - Ignatian Center for Jesuit Education</title>
+
+	<?php require('../includes/head.php'); ?>
+  </head>
+  <body class="black-nav centers tall-header">
+    <a class="sr-only sr-only-focusable" href="#content">Skip to main content</a>
+   <header class="site-header site-header-custom-title">
+      <?php require('../includes/header-ignatian-center.php'); ?>
+      <?php require('../includes/nav-ignatian-center.php'); ?>
+    </header>
+    
+    <div class="project-title">
+      <div class="container">
+				<h2>Thriving Neighbors</h2>
+      </div>      
+    </div>
+    
+      <div class="container" id="content">
+        
+      <div class="row">
+        <div class="col-sm-8">
+          <h1 class="page-title">Madres Walking to Health</h1>
+          <div class="intro">
+            <p>Through the “Madres Walking to Health” program, Kathryn Saxton and Laura Chyu of Santa Clara University’s Public Health Program propose to develop a sustainable community health program for the parents and their children at Washington Elementary School in San Jose.</p>
+          </div>
+					<div class="two-column feature-gallery module">          
+							<div id="feature-gallery-example" class="carousel slide" data-ride="carousel">
+							  <div class="carousel-inner" role="listbox"><!-- Wrapper for slides -->
+							    <div class="item active">
+							      <a href="#" class="thumbnail"><img src="Madres-programpage.jpg" alt="Madres Photo 1" width="750" height="365"></a>
+							      <div class="carousel-caption">
+							        <p>Caption needed for this photo.</p>
+							      </div>
+							    </div>
+							    <div class="item">
+							      <a href="#" class="thumbnail"><img src="Madres-programpage2.jpg" alt="Madres Photo 2" width="750" height="365"></a>
+							      <div class="carousel-caption">
+							        <p>Caption needed for this photo.</p>
+							      </div>
+							    </div>
+							  </div>
+							
+							  <!-- Controls -->
+							  <a class="left carousel-control" href="#feature-gallery-example" role="button" data-slide="prev">
+							    <span class="glyphicon glyphicon-triangle-left" aria-hidden="true"></span>
+							    <span class="sr-only">Previous</span>
+							  </a>
+							  <a class="right carousel-control" href="#feature-gallery-example" role="button" data-slide="next">
+							    <span class="glyphicon glyphicon-triangle-right" aria-hidden="true"></span>
+							    <span class="sr-only">Next</span>
+							  </a>
+							</div>
+						</div>   
+                
+          <p>“Madres Walking to Health” focuses on supporting and expanding upon a walking group that already exists within the <em>Madre a Madre</em> group at Washington Elementary. The group currently has relatively low participation and only meets twice a month. Chyu and Saxton hope to not only help grow this program, but also to help extend the collaborative, community spirit among the parents of Washington Elementary to focus on health and well-being.</p>
+
+<p>Parents in the <em>Madre a Madre</em> group at Washington have expressed their interest in increasing physical activity for themselves as well as their children. Walking groups are a relatively simple, cost-effective way for parents to promote physical activity and have consistently demonstrated significant health benefits. A neighborhood assessment by the Santa Clara County Public Health Department also revealed that most streets in the neighborhood are well lit with easily accessible sidewalks and crosswalks, making the neighborhood a relatively safe location for walking. After mothers identify the safest walking routes, participating Santa Clara University students will help create maps and collaborate with mothers on a regular walking schedule of at least 2-3 sessions per week. Participants who agree to commit to the program for at least three months will be given pedometers to help log their steps. The goal is that following the 18-month grant, the walking group activities will be managed and sustained by the parents of the <em>Madres a Madres</em> group at Washington Elementary.</p>
+
+
+          <div class="row">
+            <div class="col-md-6">
+	           <h3>Program Updates</h3>
+	           <div class="one-column news module">
+		            <ul class="media-list">
+		              <li class="media">
+		                <div class="media-body">
+		                  <h4 class="media-heading"><a href="#">2015 Kickoff Event</a></h4>
+		                  <p>Summary for this story.</p>
+		                </div>
+		              </li>
+		            </ul>
+		          </div>
+            </div>
+            <div class="col-md-6">
+	            <h3>Links and Downloads</h3>
+		         <div class="one-column related-links module">
+	              <ul>
+	                <li><a href="#">Full Project Prospectus</a></li>
+	                <li><a href="#">Research Documentation</a></li>
+	                <li><a href="#">Etc.</a></li>
+	              </ul>
+	            </div>
+            </div>
+          </div>
+
+
+        </div>
+        <div class="col-sm-4">
+          <nav class="side-navigation match-page-title">
+						<ul>
+							<li><a href="#">About</a>
+								<ul>
+									<li><a href="#">Vision</a></li>
+									<li><a href="#">Strategies</a></li>
+									<li><a href="#">Impact</a></li>
+									<li><a href="#">History</a></li>
+								</ul>
+							</li>
+							<li><a href="#">The Community</a></li>
+							<li><a href="#">Programs</a>
+								<ul>
+									<li><a href="#">Program Archive</a></li>
+								</ul>
+							</li>
+							<li><a href="#">Faculty Project Funding</a></li>
+							<li><a href="#">Get Involved</a>
+								<ul>
+									<li><a href="#">Support Us</a></li>
+									<li><a href="#">Volunteer</a></li>
+									<li><a href="#">Partner Agencies</a></li>
+									<li><a href="#">Contact Us</a></li>
+								</ul>
+							</li>
+							<li><a href="#">News &amp; Events</a>
+								<ul>
+									<li><a href="#">News Archive</a></li>
+								</ul>
+							</li>
+						</ul>            
+          </nav>
+          <div class="one-column related-links module">
+            <h2>Who's Involved?</h2>
+             <div class="one-column related-links module">
+              <h4 class="related-links-header">SCU Faculty</h4>
+              <ul>
+                <li><a href="#">Laura Chyu</a>, Department of _____</li>
+                <li><a href="#">Kat Saxton</a>, Department of _____</li>
+              </ul>
+              <h4 class="related-links-header">Community Member Co-authors</h4>
+              <ul>
+                <li>Jazmín Ballesteros, Juana Escamilla, Jessica López, Eva Marrón, Arcelia Ramírez</li>
+              </ul>
+              <h4 class="related-links-header">SCU Student Contributors</h4>
+              <ul>
+                <li>Mariam Kahn, Misja Ilcisin</li>
+              </ul>
+            </div>
+          </div>
+          <div class="one-column video module">
+            <div class="embed-responsive embed-responsive-16by9">
+              <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/XiEUKGmKmZI" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+            </div>
+            <p class="video-caption">Video: <a href="https://www.youtube.com/watch?v=XiEUKGmKmZI">Meet los Madres</a></p>
+          </div>
+        </div>
+      </div>
+
+      </div>
+ 	<?php require('../includes/footer.php'); ?>
+  </body>
+</html>
