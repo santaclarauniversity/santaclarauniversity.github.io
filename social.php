bb1e69d896a5daf3fe7b26f5e93041c081113878
jasonpontius <jason@whitewhale.net>
2016-01-11 11:15:51 -0800

    Social media icons
    Using Fontello, an open source tool. All needed files are included, and
    instructions are in as comments to social.less.  Included are Facebook,
    LinkedIn, Twitter, Instagram, Alumni Directory, Illuminate, YouTube,
    Flickr, Vimeo, Soundcloud, RSS, and  Pinterest.  It’s easy to add more
    to this default set at fontello.com.

---
+++ social.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Social icons</title>
+
+	<?php require('includes/head.php'); ?>
+  </head>
+  <body>
+	<?php require('includes/header.php'); ?>
+  
+
+      <div class="container" id="content">
+        
+
+      <div class="row">
+        <div class="col-sm-8">
+          <h1 class="page-title">Social icons</h1>
+          
+					<div class="one-column social-grid module">
+						<div class="social">
+							<a class="facebook" href="LINK"><i class="demo-icon icon-facebook-squared"></i></a>
+						</div>
+						<div class="social">
+							<a class="twitter" href="LINK"><i class="demo-icon icon-twitter"></i></a>
+						</div>
+						<div class="social">
+							<a class="youtube" href="LINK"><i class="demo-icon icon-youtube"></i></a>
+						</div>
+						<div class="social">
+							<a class="pinterest" href="LINK"><i class="demo-icon icon-pinterest"></i></a>
+						</div>
+						<div class="social">
+							<a class="vimeo" href="LINK"><i class="demo-icon icon-vimeo"></i></a>
+						</div>
+						<div class="social">
+							<a class="instagram" href="LINK"><i class="demo-icon icon-instagram"></i></a>
+						</div>
+						<div class="social">
+							<a class="rss" href="LINK"><i class="demo-icon icon-rss"></i></a>
+						</div>
+						<div class="social">
+							<a class="flickr" href="LINK"><i class="demo-icon icon-flickr"></i></a>
+						</div>
+						<div class="social">
+							<a class="soundcloud" href="LINK"><i class="demo-icon icon-soundcloud"></i></a>
+						</div>
+						<div class="social">
+							<a class="linkedin" href="LINK"><i class="demo-icon icon-linkedin"></i></a>
+						</div>
+						<div class="social">
+							<a class="illuminate" href="LINK"></a>
+						</div>
+						<div class="social">
+							<a class="scu" href="LINK"></a>
+						</div>
+					</div>
+
+        </div>
+      </div>
+
+      </div>
+      
+	<?php require('includes/footer.php'); ?>
+  </body>
+</html>
