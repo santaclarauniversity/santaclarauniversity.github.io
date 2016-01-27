89873df91b3c5eb1243d906ae0cd3c2de3a57181
jasonpontius <jason@whitewhale.net>
2016-01-27 14:57:34 -0800

    Font sizes on "can we" feature at assorted breakpoints (#35)
    Addresses #35.  Please review the result here:
    http://scu.babywhale.net/can-we.php

---
+++ can-we.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SCU | SCU Templates and Modules | Can We Features</title>
+
+	<?php require('includes/head.php'); ?>
+  </head>
+  <body> 
+	<?php require('includes/header.php'); ?>
+
+<div class="can-we feature module" style="background-image:url(http://scu.edu/media/institutional-pages/homepage/lg-hollywood.jpg); background-position:50% 10%;">
+      <div class="container can-we-body">
+	    
+		    
+        <div class="can-we-headline">
+          <a href="/cas/communication/"><span>Department of Communication</span></a>
+		    <h2><a href="/news-and-events/homepage-stories/in-the-spotlight.html">In the Spotlight</a></h2>
+	    </div>
+        <div class="row can-we-boxes" style="left: auto;">
+          <div class="col-xs-4">
+	          <div class="can-we-box" style="width: auto;"><h2><span>
+              
+	            Can we?
+            
+            </span></h2>
+            
+	            <div class="can-we-photo">
+  <img src="http://scu.edu/media/institutional-pages/homepage/ackerman-andy.jpg" alt="">
+</div>
+            
+	            <p>The studios at Santa Clara are a great place to learn the skills needed to&nbsp;<a href="https://magazine.scu.edu/scm/webonly.cfm?b=439&amp;c=21643">direct stars like Jerry Seinfeld</a>.</p>
+	            <div class="can-we-credit">
+	                <p>Andy Ackerman ’78,<br>Television Director</p>
+	            </div>
+	          </div>
+          </div>
+          
+          <div class="col-xs-4">
+	          <div class="can-we-box" style="width: auto;"><h2><span>
+              
+	            Should we?
+            
+            </span></h2>
+	            <div class="can-we-photo">
+  <img src="http://scu.edu/media/institutional-pages/homepage/mahamdi.jpg" alt="">
+</div>
+	            <p>Should&nbsp;we tackle issues like <a href="/cas/communication/faculty-and-staff/mahamdi.html">diversity on college campuses</a>&nbsp;through an art that's widely seen as entertainment?</p>
+	            <div class="can-we-credit">
+	                <p>Yahia Mahamdi,<br><a href="/cas/communication/">Communication</a></p>
+	            </div>
+	          </div>
+          </div>
+          
+          <div class="col-xs-4">
+	          <div class="can-we-box" style="width: auto;"><h2><span>
+              
+	            Will we?
+            
+            </span></h2>
+	            <div class="can-we-photo">
+  <img src="http://scu.edu/media/institutional-pages/homepage/faust-blye.jpg" alt="">
+</div>
+	            <p>What can you accomplish when you're willing to give a <a href="https://magazine.scu.edu/scm/webonly.cfm?b=439&amp;c=22909">good but difficult story</a>&nbsp;support and a careful push?</p>
+	            <div class="can-we-credit">
+	                <p>Blye Faust ’97,<br>Movie Producer</p>
+	            </div>
+	          </div>
+          </div>
+        </div>
+      </div>     
+      <div class="carousel-caption container"><p>Producer of the&nbsp;<a href="https://magazine.scu.edu/scm/webonly.cfm?b=439&amp;c=22909">Academy Award-nominated movie <em>Spotlight</em></a> <strong>Blye Faust ’97</strong>&nbsp;and communication professor <a href="/cas/communication/faculty-and-staff/whalen.html">Michael Whalen</a>&nbsp;are helping SCU students gain Hollywood experience.</p>
+      </div>
+    </div>
+
+    <div class="can-we feature module vertical" style="background-image:url(_examples/ghana.jpg); background-position:50% 10%;">
+      <div class="container can-we-body">
+	    <div class="can-we-headline">
+		    <span>Engineering the Future</span>
+		    <h2>Spring Break in Ghana</h2>
+	    </div>
+        <div class="row can-we-boxes">
+          <div class="col-xs-4">
+	          <div class="can-we-box">
+	            <h2><span>Can we?</span></h2>
+	            <div class="can-we-photo">
+		            <img src="_examples/chris-kitts.jpg"/>
+	            </div>
+	            <p>We’re helping local teams on the ground track river erosion using solar-powered drones.</p>
+	            <div class="can-we-credit">
+	                Chris Kitts<br/>
+	                Robotics & Engineering
+	            </div>
+	          </div>
+          </div>
+          <div class="col-xs-4">
+	          <div class="can-we-box">
+	            <h2><span>Should we?</span></h2>
+	            <div class="can-we-photo">
+		            <img src="_examples/chris-kitts.jpg"/>
+	            </div>
+	            <p>This is a shorter sentence.</p>
+	            <div class="can-we-credit">
+	                Chris Kitts<br/>
+	                Robotics & Engineering
+	            </div>
+	          </div>
+          </div>
+          <div class="col-xs-4">
+	          <div class="can-we-box">
+	            <h2><span>Will we?</span></h2>
+	            <div class="can-we-photo">
+		            <img src="_examples/chris-kitts.jpg"/>
+	            </div>
+	            <p>We’re helping local teams on the ground track river erosion using solar-powered drones.  This is too long.</p>
+	            <div class="can-we-credit">
+	                Chris Kitts<br/>
+	                Robotics & Engineering
+	            </div>
+	          </div>
+          </div>
+        </div>
+      </div>     
+      <div class="carousel-caption container">
+        <p>PHOTO: SCU's Mobile Mission Control lab can be found scanning the heavens from various places on campus.</p>
+      </div>
+    </div>
+    
+    
+    
+      
+	<?php require('includes/footer.php'); ?>
+  </body>
+</html>
