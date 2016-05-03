231da21fadfb1cc6f726b5cc3faa927abc5e437c
Naomi Royall <naomiroyall@gmail.com>
2016-05-02 18:51:43 -0700

    Begin work on Overheard at SCU

---
+++ includes/header-overheard.php
@@ -0,0 +1,197 @@
+<header class="site-header site-header-custom-title">
+  <div class="site-header-text-and-search">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12 site-header-titles">
+
+          <a class="wordmarkshidethis" href="/alumni/">
+            <img style="max-width:100%;height:auto;" src="https://www.scu.edu/media/offices/alumni/alumni_logo-635x200.png" alt="Alumni" width="635" height="200">
+
+          </a>
+        </div>
+        <div class="col-xs-12 col-sm-3 col-md-1 hidden-md hidden-lg  clearfix site-header-search-and-toggles">
+          <button type="button" class="visible-xs-block menu-toggle menu-toggle-main" data-toggle="offcanvas" data-target="#scu-main-navigation">
+            <span class="caret left-caret"></span>
+            <span class="menu-label">
+              <span class="sr-only">Toggle navigation</span>
+              Menu
+            </span>
+          </button>
+          <button type="button" class="visible-xs-block menu-toggle menu-toggle-gateways" data-toggle="offcanvas" data-target="#scu-gateway-navigation">
+            <span class="menu-label text-hide">
+              <span class="sr-only">Toggle navigation</span>
+              Gateways
+            </span>
+            <span class="caret right-caret"></span>
+          </button>
+          <form class="pull-right site-header-search" id="header-search-form" method="get" action="/search/">
+            <div class="input-group">
+              <input type="search" class="form-control" name="q" id="header-search" placeholder="SEARCH">
+              <span class="input-group-btn">
+                <button class="btn btn-default" type="submit" aria-label="Submit">
+                  <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+                </button>
+              </span>
+            </div>
+          </form>
+
+        </div>
+      </div>
+    </div>
+  </div>
+  <nav class="main-navigation offcanvas-left affix-top" id="scu-main-navigation"><div class="container sidebar-offcanvas"><ul class="nav nav-pills"><li class="dropdown" id="toplink11273"><a href="/alumni/events/">Events</a><ul class="dropdown-menu"><li><a href="/alumni/events/eventcalendar/">Event Calendar</a></li><li><a href="/alumni/events/reunions/">Grand Reunion</a></li><li><a href="/alumni/events/presidentsdinner/">The President's Dinner</a></li><li><a href="/alumni/events/vintage/">Vintage Santa Clara</a></li><li><a href="/alumni/events/pasta-feed-and-bronco-legends-celebration/">Pasta Feed and Bronco Legends Celebration</a></li><li><a href="/alumni/events/gradpicnic/">Graduation Picnic</a></li><li><a href="/alumni/events/spiritual/">Spiritual Events</a></li></ul></li><li class="dropdown" id="toplink5058"><a href="/alumni/connect/">Connect</a><ul class="dropdown-menu"><li><a href="/alumni/connect/socialnetworks/">Social Networks</a></li><li><a href="/alumni/connect/map/">Alumni Map</a></li><li><a href="/alumni/connect/regionalchapters/">Regional Chapters</a></li><li><a href="/alumni/connect/ethnicchapters/">Ethnic Chapters</a></li><li><a href="/alumni/connect/gradalumni/">Graduate School Alumni</a></li><li><a href="/alumni/connect/youngalumni/">Young Alumni</a></li><li><a href="/alumni/connect/students/">Student Programs</a></li><li><a href="/alumni/connect/superbroncos/">Super Broncos</a></li><li><a href="/alumni/connect/campusresources/">Campus Resources</a></li></ul></li><li class="dropdown" id="toplink5156"><a href="/alumni/serve/">Serve</a><ul class="dropdown-menu"><li><a href="/alumni/serve/volunteer/">Volunteer Interest Form </a></li><li><a href="/alumni/serve/communityservice/">Community Service</a></li><li><a href="/alumni/serve/admissions/">Admissions Support</a></li><li><a href="/alumni/serve/chapterleadership/">Chapter Leadership</a></li><li><a href="/alumni/serve/reunioncommittees/">Grand Reunion Committees</a></li><li><a href="/alumni/serve/awards/">Awards</a></li></ul></li><li class="dropdown" id="toplink5136"><a href="/alumni/learn/">Learn</a><ul class="dropdown-menu"><li><a href="http://www.scu.edu/illuminate">Illuminate</a></li><li><a href="/alumni/learn/careers/">Career Support</a></li><li><a href="/alumni/learn/takeaclass/">Take a Class</a></li><li><a href="/alumni/learn/travel/">Alumni Travel Program</a></li><li><a href="/alumni/events/spiritual/other-spiritual-opportunities/">Spiritual Opportunities</a></li></ul></li><li class="dropdown" id="toplink5150"><a href="/alumni/news/">News</a><ul class="dropdown-menu"><li><a href="/alumni/news/broncoconnect/">Bronco Connect</a></li><li><a href="/alumni/news/photos/">Photo Galleries</a></li><li><a href="/alumni/news/videos/">Videos</a></li></ul></li><li class="dropdown" id="toplink5033"><a href="/alumni/about/">About</a><ul class="dropdown-menu"><li><a href="/alumni/about/historytraditions/">History &amp; Traditions</a></li><li><a href="/alumni/about/notable/">Notable Alumni</a></li><li><a href="/alumni/about/strategicplan/">Alumni Strategic Plan</a></li><li><a href="/alumni/about/board/">Board of Directors</a></li><li><a href="/alumni/about/alumnifamilyscholarship/">Alumni Family Scholarship</a></li><li><a href="/alumni/about/gianera/">Gianera Society</a></li><li><a href="/alumni/about/staff/">Staff</a></li><li><a href="/alumni/about/faq/">Frequently Asked Questions</a></li><li><a href="https://mysantaclara.scu.edu/AlumniSupport">Support SCU</a></li></ul></li></ul></div></nav>
+  <nav class="gateway-navigation offcanvas-right" id="scu-gateway-navigation">
+    <div class="container sidebar-offcanvas">
+      <ul class="nav navbar-left">
+        <li class="dropdown scu-nav">
+          <a href="/" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+            <span class="text-hide mission">Main Navigation for</span>
+            <span class="hidden-sm">Santa Clara University</span>
+<span class="hidden-xs hidden-md hidden-lg">SCU</span>
+
+
+            <span class="caret hidden-xs"></span>
+          </a>
+          <div class="dropdown-menu">
+            <div class="container">
+              <ul role="menu" class="nav nav-pills">
+                <li><a href="/">Home</a></li>
+                <li><a href="/aboutscu/">About SCU</a></li><li><a href="/academics/">Academics</a></li><li><a href="/admission/">Admission</a></li><li><a href="/campus-life/">Campus Life</a></li><li><a href="/athletics/">Athletics</a></li><li><a href="/global/">Global</a></li><li><a href="/giving/">Giving</a></li><li><a href="/news-and-events/">News &amp; Events</a></li>
+              </ul>
+            </div>
+          </div>
+        </li>
+      </ul>
+      <ul class="nav nav-pills navbar-right">
+            <!-- Events  -->
+        <li><a href="/family/">Families</a></li>
+        <li><a href="https://mysantaclara.scu.edu/AlumniSupport">Support SCU</a></li>
+        <li class="dropdown scu-gateways">
+          <a href="#" class="dropdown-toggle text-hide hidden-xs people-gateways" data-toggle="dropdown" role="button" aria-expanded="false">People Gateways</a>
+          <div class="dropdown-menu">
+            <div class="container">
+              <ul role="menu" class="nav nav-pills navbar-right">
+                <li><a href="/students/">Students</a></li><li><a href="/faculty/">Faculty &amp; Staff</a></li><li><a href="/family/">Families</a></li><li><a href="/alumni/">Alumni</a></li><li><a href="/visitors/">Visitors</a></li>
+              </ul>
+            </div>
+          </div>
+        </li>
+        <li class="slide-menu">
+          <a href="/services/" role="button" aria-expanded="false">
+            <span class="gateway-dropdown">
+              Offices &amp; Services <span class="caret"></span>
+            </span>
+          </a>
+          <div class="slide-panel">
+            <div class="container">
+              <div class="row">
+                  <div class="col-sm-12">
+                    <h6>Offices and Services at Santa Clara University</h6>
+                  </div>
+                  <div class="col-sm-3">
+                    <ul>
+                      <li><a href="/onestop/">Enrollment Services</a></li>
+                      <li><a href="/cm/">Campus Ministry</a></li>
+                      <li><a href="/university-operations/campus-safety/">Campus Safety</a></li>
+                      <li><a href="/university-operations/facilities/">Facilities</a></li>
+                    </ul>
+                  </div>
+                  <div class="col-sm-3">
+                    <ul>
+                      <li><a href="/diversity/">Diversity and&nbsp;Inclusion</a></li>
+                      <li><a href="/technology/">Technology at SCU</a></li>
+                      <li><a href="/recreation/">Recreation</a></li>
+                      <li><a href="/sustainability/">Sustainability</a></li>
+                    </ul>
+                  </div>
+                  <div class="col-sm-3">
+                    <ul>
+                      <li><a href="/arts/">Arts at SCU</a></li>
+                      <li><a href="/auxiliary-services/bookstore/">Bookstore</a></li>
+                      <li><a href="/careercenter/">Career Center</a></li>
+                      <li><a href="/hr/">Human Resources</a></li>
+                    </ul>
+                  </div>
+                  <div class="col-sm-3">
+                    <ul>
+                    <li><a href="/library/">University Library</a></li>
+                    <li><a href="/missionchurch/">Mission Church</a></li>
+                    <li><a href="/login/">SCU Login</a></li>
+                    </ul>
+                  <a href="/services/" class="more-link">Offices and Services »</a>
+                </div>
+            </div>
+            </div>
+          </div>
+      </li>
+
+            <li class="slide-menu">
+  <a href="/site-assets/header/schools--centers/" role="button" aria-expanded="false">
+    <span class="gateway-dropdown">
+      Schools &amp; Centers <span class="caret"></span>
+    </span>
+  </a>
+  <div class="slide-panel">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-sm-7">
+          <h6>Schools and Colleges</h6>
+          <div class="row">
+            <div class="col-sm-6">
+              <ul>
+<li><a href="/cas/">College of Arts and&nbsp;Sciences</a></li>
+<li><a href="/ecp/">Education and Counseling Psychology</a></li>
+<li><a href="/business/">Leavey School of Business</a></li>
+</ul>
+            </div>
+            <div class="col-sm-6">
+              <ul>
+<li><a href="/engineering/">School of Engineering</a></li>
+<li><a href="/jst/">Jesuit School of Theology</a></li>
+<li><a href="http://law.scu.edu">School of Law</a></li>
+</ul>
+
+             </div>
+          </div>
+        </div>
+
+        <div class="col-sm-5">
+          <h6> Centers of Distinction</h6>
+          <div class="row">
+            <div class="col-sm-12">
+              <ul>
+<li><a href="http://www.scu-social-entrepreneurship.org/">Miller Center for Social Entrepreneurship</a></li>
+<li><a href="/ic/">Ignatian Center for Jesuit Education</a></li>
+<li><a href="/ethics/">Markkula Center for Applied Ethics</a></li>
+</ul><a href="/centers/" class="more-link">Centers and Institutes »</a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</li>
+
+        <li class="hidden-xs dropdown dropdown-search">
+          <a href="#" class="glyphicon glyphicon-search" data-toggle="dropdown" role="button" aria-expanded="false"></a>
+          <div class="dropdown-menu">
+            <form class="navbar-form" method="get" action="/search/">
+              <div class="input-group">
+                <input type="search" class="form-control" name="q" id="q" placeholder="SEARCH">
+                <span class="input-group-btn">
+                  <button class="btn btn-default" type="submit" aria-label="Submit"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
+                </span>
+              </div>
+
+            </form>
+          </div>
+        </li>
+
+      </ul>
+    </div>
+    <div class="slide-menu-backdrop"></div>
+  </nav>
+
+</header>
---
+++ overheard.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SCU | SCU Alumni | Overhead @ SCU</title>
+
+	  <?php require('includes/head.php'); ?>
+    <link href="https://www.scu.edu/css/scu.css" rel="stylesheet">
+    <link href="https://www.scu.edu/media/scuedu/style-assets/stylesheets/scu-add.css" rel="stylesheet">
+    <link href="/css/overheard.css" rel="stylesheet">
+  </head>
+  <body class="color-burgundy-alumni font-bodoni  white-nav white-nav">
+	<?php require('includes/header-overheard.php'); ?>
+
+  <div class="container" id="content">
+
+    <div class="row">
+      <div class="col-sm-8">
+        <div class="overheard-demo-container">
+
+          <h2>Illuminate</h2>
+          <aside class="overheard mod-illuminate mod-large">
+            <h5 class="overheard-title"><strong>Overheard</strong><span>@</span>SCU</h5>
+            <div class="overheard-box">
+              <div class="overheard-main">
+                <blockquote>This article inspires me to keep going and strive for better.</blockquote>
+              <footer class="overheard-footer">
+                <img src="/css/assets/overheard-illuminate-branding.png" alt="Illuminate"/>
+              </footer>
+            </div>
+            </div>
+          </aside>
+
+          <p>Illuminate: short quote with Illuminate branding</p>
+
+          <aside class="overheard mod-illuminate">
+            <h5 class="overheard-title"><strong>Overheard</strong><span>@</span>SCU</h5>
+            <div class="overheard-box">
+              <div class="overheard-main">
+                <blockquote>Beautiful piece, and timely too! This has left me inspired. (This illustrates the style used for longer quotes and can be used for up to 120 - 160 characters).</blockquote>
+              </div>
+              <footer class="overheard-footer">
+                <img src="/css/assets/overheard-illuminate-branding.png" alt="Illuminate"/>
+              </footer>
+            </div>
+          </aside>
+
+          <p>Illuminate: long quote with Illuminate branding</p>
+
+          <aside class="overheard mod-illuminate">
+            <h5 class="overheard-title"><strong>Overheard</strong><span>@</span>SCU</h5>
+            <div class="overheard-box">
+              <div class="overheard-main">
+                <blockquote>Beautiful piece, and timely too! This has left me inspired. (This illustrates the style used for much longer quotes and can be used for up 190 characters without the Illuminate footer).</blockquote>
+              </div>
+            </div>
+          </aside>
+
+          <p>Illuminate: quote only</p>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <nav class="side-navigation match-page-title">
+          <a class="side-navigation-header" href="index.php">SCU Templates and Modules</a>
+          <ul>
+            <li><a href="homepage.php">Homepage</a></li>
+            <li><a href="inside-two-columns.php">Inside Page: 2 Columns</a></li>
+            <li><a href="inside-three-columns.php">Inside Page: 3 Columns</a></li>
+            <li><a href="inside-full-width.php">Inside Page: Full Width</a></li>
+            <li><a href="inside-grid.php">Inside Page: Grid</a></li>
+            <li><a href="inside-jumbotron-three-columns.php">Inside Page: Jumbotron Three Columns</a></li>
+            <li><a href="inside-jumbotron-two-columns.php">Inside Page: Jumbotron Two Columns</a></li>
+            <li>
+              <a href="one-column-modules.php">One Column Modules</a>
+              <ul class="multilevel-linkul-0">
+                <li><a href="building-one-column-modules.php">Building One Column Modules</a></li>
+                <li><a href="#">A test link</a></li>
+              </ul>
+            </li>
+            <li>
+              <a href="two-column-modules.php">Two Column Modules</a>
+            </li>
+            <li>
+              <a href="full-width-modules.php">Full Width Modules</a>
+              <ul class="multilevel-linkul-0">
+                <li><a href="can-we.php">Hero Features</a></li>
+                <li><a href="hero-features.php">Hero Features</a></li>
+                <li><a href="landing-hero-features.php">Landing Hero Features</a></li>
+                <li><a href="subheaders.php">Subheaders</a></li>
+              </ul>
+            </li>
+            <li class="active">
+              <a href="headers.php">Headers</a>
+              <ul class="multilevel-linkul-0">
+                <li><a href="header-core-nav-core-gateways.php">Core Nav, Core Gateways</a></li>
+                <li class="active"><a href="header-site-nav-site-gateways.php">Site Nav, Site Gateways</a>
+                  <ul class="multilevel-linkul-1">
+                    <li><a href="header-stone-site-nav.php">Example: Stone Nav</a></li>
+                    <li class="active"><a href="header-gray-site-nav.php">Example: Gray Nav</a></li>
+                    <li><a href="header-black-site-nav.php">Example: Black Nav</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+
+  </div>
+
+	<?php require('includes/footer.php'); ?>
+  </body>
+</html>
