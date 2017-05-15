dec88b6fea8b41f9dd4461647de55174ce2343d7
Nic Bertino <nbertino@scu.edu>
2017-05-15 16:18:29 -0700

    Refactored toolkit.js back to es2015

--- src/assets/toolkit/scripts/toolkit.js
+++ src/assets/toolkit/scripts/toolkit.js
@@ -1,29 +1,17 @@
-// TODO Revisit to fix Webpack loading required external JS
-/*require('imports-loader?$=jquery!bootstrap/js/dist/util');
-require('imports-loader?$=jquery!bootstrap/js/dist/collapse');
-require('imports-loader?$=jquery!bootstrap/js/dist/carousel');
-require('imports-loader!holderjs/holder');*/
-
-function searchEsc(e) {
-  if (e.which === 27) {
-    e.preventDefault();
-
-    $('.fa-search').click();
-  }
-}
-
 /**
  * Header and footer button functionality
  */
-$(document).ready(() => {
+$(function () {
   // TODO temp: toggle subheader/normal header
-  $('.header-toggler').click(() => {
+
+  $('.header-toggler').click(function () {
+    console.log("triggered");
     $('.core-nav').toggle();
     $('.compact-nav').toggle();
   });
 
   // toggles search overlay
-  $('.fa-search').click((e) => {
+  $('.fa-search').click(function (e) {
     // TODO fix event listener not removing
     e.preventDefault();
 
@@ -32,109 +20,57 @@ $(document).ready(() => {
 
     if ($('.search-module--open')) {
       document.addEventListener("keyup", searchEsc);
-    } else {
-      console.log('removed');
-      document.removeEventListener("keyup", searchEsc);
     }
   });
 
   // closes search via X button
-  $('.close-btn').click((e) => {
+  $('.close-btn').click(function (e) {
     e.preventDefault();
 
     $('.drawer').removeClass('open');
     $('.search-module').removeClass('search-module--open');
-    document.removeEventListener("keyup", searchEsc);
   });
 
   // toggles quick links drawer
-  $('.fa-map').click((e) => {
+  $('.fa-map').click(function (e) {
     e.preventDefault();
-
     $('.drawer').toggleClass('open');
+    document.addEventListener("keyup", searchEsc);
   });
 
   // dept switcher
-  $(document).ready(function () {
-    $('#departments').select2({
-      theme: "bootstrap",
-      placeholder: 'Departments and Programs',
-    });
-    $('#departments').on('select2:select', function (evt) {
-      var dest = $(evt.params.data.element).data('target');
-      // window.location = dest;
-    });
+  $('.custom-select').select2({
+    theme: "bootstrap",
+    placeholder: 'Departments and Programs'
+  }).on('select2:select', function (evt) {
+    var dest = $(evt.params.data.element).data('target');
+    // window.location = dest;
   });
 
-  $('#footer-toggler').click(() => {
+  $('#footer-toggler').click(function () {
     $('.footer-core').toggle();
     $('.footer-subfooter').toggle();
   });
 });
 
-/**
- * Feature Gallery / Jumbotron
- * Adding swipe events to Bootstrap's Carousel
- * Requires jquery.event.swipe.js, move and swipe libraries
- * by Stephen Band http://stephband.info/jquery.event.swipe/
- */
-// Adding swipe events to Jumbotrons and Feature Galleries:
-$(document).ready(() => {
-  $('.carousel.slide')
-    .carousel({
-      interval: false, // Knock off the autoplay
-    })
-    .carousel('pause')
-    .on('swipeleft', () => {
-      $(this).carousel('next');
-    })
-    .on('swiperight', () => {
-      $(this).carousel('prev');
-    })
-    .on('movestart', function (e) {
-      if ((e.distX > e.distY && e.distX < -e.distY) ||
-        (e.distX < e.distY && e.distX > -e.distY)) {
-        e.preventDefault();
-      }
-    });
-});
-
-/**
- * Jumbotron Feature
- */
-$(() => {
-  const $gallery = $('.gallery.module');
-  const $image = $gallery.find('.full-image .gallery-image img');
-
-  $gallery.on('click', '.slides .gallery-image', function (e) {
-    e.preventDefault();
-
-    const $this = $(this);
-    const imgSrc = $this.find('.thumbnail img').attr('src');
-    const caption = $this.find('.gallery-caption').html() || '';
-
-    console.log($this.find('.gallery-image img').length);
-
-    $image
-      .fadeOut(100, () => {
-        $image.attr('src', imgSrc);
-      })
-      .fadeIn(100);
-
-    caption.html(caption);
-
-    return true;
-  });
-});
 
 /**
  * Department Switcher
  */
-$(() => {
-  const $switcher = $('.switcher');
+$(function () {
+  var $switcher = $('.switcher');
 
-  $switcher.on('click', () => {
+  $switcher.on('click', function () {
     $switcher.toggleClass('on');
     $switcher.toggleClass('two-column');
   });
 });
+
+function searchEsc(e) {
+  if (e.which === 27) {
+    e.preventDefault();
+    document.removeEventListener("keyup", searchEsc);
+    $('.search-module').removeClass('search-module--open');
+    $('.drawer').removeClass('open');
+  }
+}
